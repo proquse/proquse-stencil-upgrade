a2fe73dd5c5200f2fdd9955616ad52a09d3165e7
Elias Eriksson <eliaseriksson95@gmail.com>
2024-06-20 10:13:15 +0200

    base init

---
+++ stencil.config.ts
@@ -0,0 +1,28 @@
+import { Config } from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'proquse-stencil-upgrade',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+      customElementsExportBehavior: 'auto-define-custom-elements',
+      externalRuntime: false,
+      generateTypeDeclarations: true,
+      
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  testing: {
+    browserHeadless: "new",
+  },
+};
